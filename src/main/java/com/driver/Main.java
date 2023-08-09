ca545c688650c4317da3de7f66981567ee430f18
Rock <rockpraneeth1999@gmail.com>
2023-08-09 23:48:05 +0530

    Implement Getters and setters Attemp 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly=new RWOnly();
+
+        //rwOnly.name="Praneeth";
+        //System.out.println(rwOnly.name);
+
+        rwOnly.setName("Praneeth");
+        System.out.println(rwOnly.getName());
+
+
+    }
 }
